0852560c7867374780f8ef224e99a4b78f6eaa68
CHAIMAA <chaimaakaine201@gmail.com>
2024-05-29 22:08:34 +0100

    LAST VERSION

--- src/main/java/org/sid/ebanckingbackend/web/CustomerRestController.java
+++ src/main/java/org/sid/ebanckingbackend/web/CustomerRestController.java
@@ -8,6 +8,7 @@ import org.sid.ebanckingbackend.dtos.CustomerDTO;
 import org.sid.ebanckingbackend.exception.BankAccountNotFoundException;
 import org.sid.ebanckingbackend.exception.CustomerNotFoundException;
 import org.sid.ebanckingbackend.services.BankAccountService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,32 +21,38 @@ public class CustomerRestController {
     private BankAccountService bankAccountService;
 
     @GetMapping("/customers")
+    @PreAuthorize("hasAnyAuthority('SCOPE_USER')")
     public List<CustomerDTO> customers() {
 
         return bankAccountService.listCustomers();
     }
 
     @GetMapping("/customers/search")
+    @PreAuthorize("hasAnyAuthority('SCOPE_USER')")
     public List<CustomerDTO> searchcustomers(@RequestParam(name="keyword",defaultValue = "") String keyword) {
         return bankAccountService.searchCustomers("%"+keyword+"%");
     }
     @GetMapping("/customers/{id}")
+    @PreAuthorize("hasAnyAuthority('SCOPE_USER')")
     public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
         return bankAccountService.getCustomer(customerId);
     }
 
     @PostMapping("/customers")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ADMIN')")
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO) {
          return bankAccountService.saveCustomer(customerDTO);
 
     }
     @PutMapping("customers/{customerId}")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ADMIN')")
     public CustomerDTO updateCustomer(@PathVariable Long customerId, @RequestBody CustomerDTO customerDTO)
     {
         customerDTO.setId(customerId);
         return bankAccountService.updateCustomer(customerDTO);
     }
     @DeleteMapping("/customers/{id}")
+    @PreAuthorize("hasAnyAuthority('SCOPE_ADMIN')")
     public void deleteCustomer(@PathVariable Long id)
     {
         bankAccountService.deleteCustomer(id);
